d616ae542845ce323deb1f3c6af936fffbb39cf4
Akashjaiswal-IT <akashjaiswal190505@gmail.com>
2025-06-06 08:35:35 +0000

    switch

---
+++ 04_control-flow/02_switch.js
@@ -0,0 +1,24 @@
+// Switch statement
+const month = "feb";
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("February");
+        break;
+
+    case "march":
+        console.log("March");
+        break;
+
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("No matching month found.");
+        break;
+}
